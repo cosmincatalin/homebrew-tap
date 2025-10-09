87e41e3bb53d526acc871da8311fab17fe271344
Cosmin Catalin SANDA <me@cosminsanda.com>
2025-10-09 12:17:32 +0200

    Update BrewExplorer to version 0.1.2

--- Formula/brew-explorer.rb
+++ Formula/brew-explorer.rb
@@ -1,9 +1,9 @@
 class BrewExplorer < Formula
     desc "A TUI for managing and reviewing in detail which Homebrew packages are installed on the system."
     homepage "https://github.com/cosmincatalin/brew-explorer"
-    url "https://github.com/cosmincatalin/brew-explorer/releases/download/v0.1.1/brew-explorer-0.1.1-aarch64-apple-darwin.tar.gz"
-    sha256 "562159d00ca5d8b7f88d8bccbd7a6bdce78ef5aa0f517e2aba27e31fa9330461"
-    version "0.1.1"
+    url "https://github.com/cosmincatalin/brew-explorer/releases/download/v0.1.2/brew-explorer-0.1.2-aarch64-apple-darwin.tar.gz"
+    sha256 "cb17a84609fdd77f1d352370c9b6380e3aacb73872c66aeb61f8ef4a52bd14ec"
+    version "0.1.2"
 
     def install
         bin.install "brew-explorer"
