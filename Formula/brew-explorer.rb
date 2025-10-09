bcbe1eea2ea159892b7c57acb1821a48c8d2b66b
Cosmin Catalin SANDA <me@cosminsanda.com>
2025-10-09 21:36:07 +0200

    Update brew-explorer.rb

--- Formula/brew-explorer.rb
+++ Formula/brew-explorer.rb
@@ -5,11 +5,11 @@ class BrewExplorer < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/cosmincatalin/brew-explorer/releases/download/v#{version}/brew-explorer-#{version}-apple-darwin-aarch64.tar.gz"
+      url "https://github.com/cosmincatalin/brew-explorer/releases/download/v#{version}/brew-explorer-#{version}-aarch64-apple-darwin.tar.gz"
       sha256 "b666260f79aa54039d5a59f22c038c32c6fab323a24634227b9f3f9e15765fa3"
     end
     on_intel do
-      url "https://github.com/cosmincatalin/brew-explorer/releases/download/v#{version}/brew-explorer-#{version}-apple-darwin-x86_64.tar.gz"
+      url "https://github.com/cosmincatalin/brew-explorer/releases/download/v#{version}/brew-explorer-#{version}-x86_64-apple-darwin.tar.gz"
       sha256 "caba818b126f59614de8e8ea87cf3ed5fe6cf5e557d1fa3e45a4382ab121ea52"
     end
   end
