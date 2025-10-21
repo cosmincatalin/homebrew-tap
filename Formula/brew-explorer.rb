7dacfa830bb973a92b5698624b1c120e2fce545f
Cosmin Sanda <cosmin.sanda@LEGO.com>
2025-10-21 18:18:50 +0200

    Revert "Bump version to 0.1.4 and update checksums"
    This reverts commit 13c9abfcf351af93fb688593ae392a3ea8cd52c0.

--- Formula/brew-explorer.rb
+++ Formula/brew-explorer.rb
@@ -1,16 +1,16 @@
 class BrewExplorer < Formula
   desc "A TUI for managing and reviewing in detail which Homebrew packages are installed on the system."
   homepage "https://github.com/cosmincatalin/brew-explorer"
-  version "0.1.4"
+  version "0.1.3"
 
   on_macos do
     on_arm do
       url "https://github.com/cosmincatalin/brew-explorer/releases/download/v#{version}/brew-explorer-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "7c282d0722deed2ce0d2d33c9ba36c6fd3eb24fd227cf5a07e4fc8e8dee23e9e"
+      sha256 "b666260f79aa54039d5a59f22c038c32c6fab323a24634227b9f3f9e15765fa3"
     end
     on_intel do
       url "https://github.com/cosmincatalin/brew-explorer/releases/download/v#{version}/brew-explorer-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "4dbb553a844bc85791fc37f27041e21002e1515cefa37fa5f198386e48e2174c"
+      sha256 "caba818b126f59614de8e8ea87cf3ed5fe6cf5e557d1fa3e45a4382ab121ea52"
     end
   end
 
