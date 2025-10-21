cbb90828c7c8d3bfc008933b44a80ea3252aca0f
Cosmin Catalin SANDA <me@cosminsanda.com>
2025-10-21 18:28:24 +0200

    Update BrewExplorer to version 0.1.4

--- Formula/brew-explorer.rb
+++ Formula/brew-explorer.rb
@@ -1,16 +1,16 @@
 class BrewExplorer < Formula
   desc "A TUI for managing and reviewing in detail which Homebrew packages are installed on the system."
   homepage "https://github.com/cosmincatalin/brew-explorer"
-  version "0.1.3"
+  version "0.1.4"
 
   on_macos do
     on_arm do
       url "https://github.com/cosmincatalin/brew-explorer/releases/download/v#{version}/brew-explorer-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "b666260f79aa54039d5a59f22c038c32c6fab323a24634227b9f3f9e15765fa3"
+      sha256 "7572a7fedcda2785febb288924c28ef18d20526a550fa6873a6d6c8b147db916"
     end
     on_intel do
       url "https://github.com/cosmincatalin/brew-explorer/releases/download/v#{version}/brew-explorer-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "caba818b126f59614de8e8ea87cf3ed5fe6cf5e557d1fa3e45a4382ab121ea52"
+      sha256 "47bc7ff2f6da38779086df21bad2504e607fe742de8bd598abc51ef4c2809759"
     end
   end
 
